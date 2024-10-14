0779ab18d9516fd4b243465f86c94ec1c02cab25
Ricardo-Alves-Dev <rpsevla@gmail.com>
2024-10-14 15:23:58 -0300

    Classes SellerDaoJDBC e Dao Factory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,47 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import mode.entities.Department;
+import mode.entities.Seller;
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
