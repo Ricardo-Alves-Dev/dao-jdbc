32cfa8245abe257c59f9038e9f23f9b928eb0400
Ricardo-Alves-Dev <rpsevla@gmail.com>
2024-10-15 14:41:11 -0300

    Imprementação delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import mode.entities.Department;
 import mode.entities.Seller;
@@ -12,6 +13,8 @@ import model.dao.SellerDao;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -32,16 +35,24 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert =====");
+		System.out.println("==== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 5: seller update =====");
+		System.out.println("==== TEST 5: seller update =====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update Completo");
+		
+		System.out.println("==== TEST 6: seller update =====");
+		System.out.println("Entre com ID a ser Deletado");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completo");
+		
+		sc.close();
 	}
 
 }
